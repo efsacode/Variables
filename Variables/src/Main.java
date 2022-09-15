a412dce62b45f3d2163a6c28b0a7291a0ad36ceb
efsa <113553752+efsacode@users.noreply.github.com>
2022-09-15 14:38:44 +0300

    Variables

---
+++ Variables/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int yas = 21;
+		String name = "efsa";
+		String bolum = "Bilgisayar Muhendisligi.";
+		
+		System.out.println("Merhaba, adim " +name +" yasim " +yas +" bolumum " +bolum );	
+	}
+
+}
